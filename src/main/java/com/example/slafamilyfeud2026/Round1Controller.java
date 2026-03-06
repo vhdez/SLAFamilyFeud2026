5317950da16aba338e71889598ef1b5e8859d365
MinghaoZhang <mzhang26@scienceleadership.org>
2026-03-06 10:25:02 -0500

    stuff1

--- src/main/java/com/example/slafamilyfeud2026/Round1Controller.java
+++ src/main/java/com/example/slafamilyfeud2026/Round1Controller.java
@@ -2,13 +2,47 @@ package com.example.slafamilyfeud2026;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+
+import javax.swing.*;
+import javax.swing.text.html.ImageView;
 
 public class Round1Controller {
     @FXML
     private Label welcomeText;
+    ImageView backgroundImage;
+    Label	team1Score;
+    Label	team2Score;
+    Label	roundScore;
+    GridPane Answers;
+   Label Answer1;
+    Label	Answer2;
+    Label	Answer3;
+    Label	Answer4;
+    Label	Answer5;
+    Label	Answer6;
+    Label	Answer7;
+    Label	Answer8;
+
+    Integer team1Score
+    Integer team2Score
+    Integer currentRoundScore;
+    Integer XsCount;
+    Integer currentQuestionNum;
+
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+
+    public void initialize() throws Exception {
+        public void selectTeam () throws Exception;
+
+    }
+
+    public void currentQuestion() throws Exception;
+}
+public void nextQuestion() throws Exception;
+
 }
