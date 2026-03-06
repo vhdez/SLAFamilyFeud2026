74607fc7b6550f3098a2d160b9690e07ecee175d
alexchancey <achancey27@scienceleadership.org>
2026-03-06 10:20:27 -0500

    added fields + getters/setts to Questions class

--- src/main/java/com/example/slafamilyfeud2026/Question.java
+++ src/main/java/com/example/slafamilyfeud2026/Question.java
@@ -1,4 +1,54 @@
 package com.example.slafamilyfeud2026;
 
+import java.util.ArrayList;
+
 public class Question {
+    public int questionNumber;
+    public String theQuestion;
+    ArrayList<Answer> theAnswers;
+    static ArrayList<Question> allTheQuestions;
+
+    public Question(int questionNumber, String theQuestion, ArrayList<Answer> theAnswers) {
+        this.questionNumber = questionNumber;
+        this.theQuestion = theQuestion;
+        this.theAnswers = theAnswers;
+    }
+
+    public int getQuestionNumber() {
+        return questionNumber;
+    }
+
+    public void setQuestionNumber(int questionNumber) {
+        this.questionNumber = questionNumber;
+    }
+
+    public String getTheQuestion() {
+        return theQuestion;
+    }
+
+    public void setTheQuestion(String theQuestion) {
+        this.theQuestion = theQuestion;
+    }
+
+    public ArrayList<Answer> getTheAnswers() {
+        return theAnswers;
+    }
+
+    public void setTheAnswers(ArrayList<Answer> theAnswers) {
+        this.theAnswers = theAnswers;
+    }
+
+    public static ArrayList<Question> getAllTheQuestions() {
+        return allTheQuestions;
+    }
+
+    public static void setAllTheQuestions(ArrayList<Question> allTheQuestions) {
+        Question.allTheQuestions = allTheQuestions;
+    }
+
+
+    public static void readQuestions(){
+
+
+    }
 }
