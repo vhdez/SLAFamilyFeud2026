723945897afeabe14e987ccdf554274f24b1855e
sam <zliu26@scienceleadership.org>
2026-03-10 14:25:51 -0400

    Controller 2 Updated

--- src/main/java/com/example/slafamilyfeud2026/Round2Controller.java
+++ src/main/java/com/example/slafamilyfeud2026/Round2Controller.java
@@ -1,5 +1,30 @@
 package com.example.slafamilyfeud2026;
 
-public class Round2Controller {
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 
+public class Round2Controller {
+    public Label phaseLabel;
+    public GridPane answersGrid;
+    public Label roundTotal;
+    public Label answer1;
+    public Label answer2;
+    public Label answer3;
+    public Label answer4;
+    public Label answer5;
+    public Label answer6;
+    public Label answer7;
+    public Label answer8;
+    public Label answer9;
+    public Label answer10;
+    public Label score1;
+    public Label score2;
+    public Label score3;
+    public Label score4;
+    public Label score5;
+    public Label score6;
+    public Label score7;
+    public Label score8;
+    public Label score9;
+    public Label score10;
 }
