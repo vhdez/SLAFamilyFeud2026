e3248188133dc8a475d6f6aa2466dd1245f0bb6c
vhdez <vhernandez@scienceleadership.org>
2026-03-12 10:52:08 -0400

    Improved typed-in WRONG answers.

--- src/main/java/com/example/slafamilyfeud2026/Round2Controller.java
+++ src/main/java/com/example/slafamilyfeud2026/Round2Controller.java
@@ -153,7 +153,6 @@ public class Round2Controller {
                 p2ScoreLabels[currentQuestionIndex].setText("?");
             }
             System.out.print(" WRONG ");
-            return;
         } else {
 
             int answerIndex = keyToIndex(key);
